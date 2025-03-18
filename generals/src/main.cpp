9030dc85e79952e12262b01d2e370698790d03fe
Victor Matia <vmatir@outlook.com>
2025-03-18 23:22:07 +0000

    Add common defines and generated version support
    Introduce `common/defines.hpp` for shared macros and setup, including
    debug handling.
    
    Add `generated.hpp.in` to manage project versioning via CMake
    configuration.
    
    Enhance SDL initialization and cleanup in `main.cpp`, and update
    `.clang-format` for improved include ordering.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

--- generals/src/main.cpp
+++ generals/src/main.cpp
@@ -2,10 +2,29 @@
     #define SDL_MAIN_USE_CALLBACKS
 #endif // SDL_MAIN_USE_CALLBACKS
 
+#include <iostream>
+#include <print>
+
+#include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+
+#include "defines.hpp"
+#include "generated.hpp"
+
+namespace {} // anonymous namespace
 
 auto SDL_AppInit([[maybe_unused]] void** appstate, [[maybe_unused]] int argc,
                  [[maybe_unused]] char** argv) -> SDL_AppResult {
+#ifdef GENERALS_AND_ZERO_HOUR_DEBUG
+    SDL_SetLogPriorities(SDL_LOG_PRIORITY_VERBOSE);
+#endif // GENERALS_AND_ZERO_HOUR_DEBUG
+
+    SDL_SetAppMetadata("C&C Generals", GENERALS_VERSION_STRING, "generals.cnc");
+
+    if (!SDL_InitSubSystem(SDL_INIT_VIDEO)) {
+        std::println("Failed to initialize SDL3: {}", SDL_GetError()); // Make a logging class
+    }
+
     return SDL_APP_CONTINUE;
 }
 
@@ -13,7 +32,13 @@ auto SDL_AppIterate([[maybe_unused]] void* appstate) -> SDL_AppResult { return S
 
 auto SDL_AppEvent([[maybe_unused]] void* appstate, [[maybe_unused]] SDL_Event* event)
     -> SDL_AppResult {
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    }
+
     return SDL_APP_CONTINUE;
 }
 
-auto SDL_AppQuit([[maybe_unused]] void* appstate, [[maybe_unused]] SDL_AppResult result) -> void {}
+auto SDL_AppQuit([[maybe_unused]] void* appstate, [[maybe_unused]] SDL_AppResult result) -> void {
+    SDL_Quit();
+}
