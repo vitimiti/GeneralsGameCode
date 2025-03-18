875afb3d0a5178177a7abf7e872118a0d922c0b8
Victor Matia <vmatir@outlook.com>
2025-03-18 23:40:35 +0000

    Add state struct and initialize SDL window in SDL_AppInit
    Introduced a `state_t` struct to encapsulate window properties and added
    SDL window creation in the `SDL_AppInit` function.
    
    Updated the project version to 0.2.0 in CMakeLists.txt to reflect these
    changes.
    
    These modifications lay the groundwork for future window management
    features.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

--- generals/src/main.cpp
+++ generals/src/main.cpp
@@ -2,6 +2,7 @@
     #define SDL_MAIN_USE_CALLBACKS
 #endif // SDL_MAIN_USE_CALLBACKS
 
+#include <cstdint>
 #include <iostream>
 #include <print>
 
@@ -11,10 +12,16 @@
 #include "defines.hpp"
 #include "generated.hpp"
 
-namespace {} // anonymous namespace
+namespace {
+struct state_t {
+    static constexpr std::int32_t window_width  = 800;
+    static constexpr std::int32_t window_height = 600;
+    SDL_Window*                   window        = nullptr;
+};
+} // anonymous namespace
 
-auto SDL_AppInit([[maybe_unused]] void** appstate, [[maybe_unused]] int argc,
-                 [[maybe_unused]] char** argv) -> SDL_AppResult {
+auto SDL_AppInit(void** appstate, [[maybe_unused]] int argc, [[maybe_unused]] char** argv)
+    -> SDL_AppResult {
 #ifdef GENERALS_AND_ZERO_HOUR_DEBUG
     SDL_SetLogPriorities(SDL_LOG_PRIORITY_VERBOSE);
 #endif // GENERALS_AND_ZERO_HOUR_DEBUG
@@ -24,6 +31,16 @@ auto SDL_AppInit([[maybe_unused]] void** appstate, [[maybe_unused]] int argc,
     if (!SDL_InitSubSystem(SDL_INIT_VIDEO)) {
         std::println("Failed to initialize SDL3: {}", SDL_GetError()); // Make a logging class
     }
+
+    // TODO: Process command line arguments for options
+    state_t                   state{};
+    constexpr SDL_WindowFlags window_flags  = SDL_WINDOW_RESIZABLE;
+    constexpr auto            actual_width  = state_t::window_width;
+    constexpr auto            actual_height = state_t::window_height;
+    state.window = SDL_CreateWindow("Command and Conquer - Generals", actual_width, actual_height,
+                                    window_flags);
+
+    *appstate = &state;
 
     return SDL_APP_CONTINUE;
 }
