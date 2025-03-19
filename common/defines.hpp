92f9966a1241614e982d02b469dc1ce6dede8808
Victor Matia <vmatir@outlook.com>
2025-03-19 14:21:20 +0000

    Update debug macros and bump project version to 0.3.2
    Added conditional `_DEBUG` definition for MSVC in debug mode to align
    with Visual Studio requirements.
    
    Incremented project version from 0.3.1 to 0.3.2 in `CMakeLists.txt`.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

--- common/defines.hpp
+++ common/defines.hpp
@@ -3,6 +3,11 @@
 
 #ifndef NDEBUG
     #define GENERALS_AND_ZERO_HOUR_DEBUG
-#endif // NDEBUG
+
+    // For MSVC / Visual Studio
+    #ifndef _DEBUG
+        #define _DEBUG
+    #endif // _DEBUG
+#endif     // NDEBUG
 
 #endif // GENERALS_AND_ZERO_HOUR_COMMON_DEFINES_HPP
